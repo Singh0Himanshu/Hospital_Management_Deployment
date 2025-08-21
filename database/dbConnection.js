7abf94a46136305e87b511f417d1e687ef829bb9
Himanshu Singh <143202589+Singh0Himanshu@users.noreply.github.com>
2025-08-21 19:06:49 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config({ path: "./config.env" });
 
 export const  dbConnection = () =>{
     mongoose.connect(process.env.MONGO_URI,{
